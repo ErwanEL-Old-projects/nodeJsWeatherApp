5f9650e159eb78dc280432aa486bf8807f01758f
ErwanEL <leblois.erwan@hotmail.fr>
2017-12-19 22:50:36 -0500

    Added errors print function,  parsing

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ const weather = require('./weather.js');
 const query = process.argv.slice(2);
 
 weather.get(query);
+//medellin: /q/zmw:00000.4.80110
--- weather.js
+++ weather.js
@@ -1,20 +1,41 @@
 const http = require('http');
 const api = require('./api.json');
 
+function print (temp, city) {
+    let message = `Hace ${temp} grados en ${city}`
+    console.log(message);
+}
+function printError (error) {
+    console.error(error);
+}
 
 function get(city) {
-    let url = `http://api.wunderground.com/api/${api.key}/conditions/q/CA/${city}.json`
-    const request = http.get(url, response => {
-        if(response.statusCode === 200) {
-            let body = "";
-                response.on('data', data => {
-                    body += data.toString();
-                });
-                response.on('end', () => {
-                    console.log(body);
-                })
-        }
-    })
+    try {
+        let url = `http://api.wunderground.com/api/${api.key}/conditions${city}.json`
+        const request = http.get(url, response => {
+            if(response.statusCode === 200) {
+                let body = "";
+                    response.on('data', data => {
+                        body += data.toString();
+                    });
+                    response.on('end', () => {
+                        try {
+                            let data = JSON.parse(body);
+                            let city = data.current_observation.display_location.city;
+                            let temp = data.current_observation.temp_c;
+                            print(temp, city);
+                        } catch (error) {
+                            printError('ville non localisée');
+                        }
+                    })
+            } else {
+                const message = `Error getting the localisation of ${city} (${http.STATUS_CODES[response.statusCode]})`
+                printError(message);            }
+        })//fin de requete
+        request.on('error', error => printError(`La syntaxe de l'url n'est pas bonne`));
+    } catch (error) {
+        printError(`Nom de domaine introuvable(https://)`);
+    }
 };
 
 module.exports.get = get;
